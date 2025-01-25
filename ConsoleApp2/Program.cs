29453b526ba88f3f6a5770c6ee7d9b2daf449454
Christopher von Blum <christopher.vonblum@gmail.com>
2025-01-25 11:20:54 +0100

    refactoring

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -9,7 +9,6 @@ class Program
     
     public static void Main(string[] args)
     {
-
         Dictionary<string, List<long>> overallResultDictionary = new Dictionary<string, List<long>>();
         
         BenchmarkAll(30, overallResultDictionary);
